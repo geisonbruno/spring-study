94911d3161c8545787f35b541a164e09da66e67e
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-04-07 22:45:29 -0300

    first commit

---
+++ src/test/java/br/com/gbruno/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gbruno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
