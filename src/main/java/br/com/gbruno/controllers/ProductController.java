6c48906ac59fb7f7eb8eda328e04a0014ad8f243
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-04-09 14:46:54 -0300

    feat: created ProductController

---
+++ src/main/java/br/com/gbruno/controllers/ProductController.java
@@ -0,0 +1,12 @@
+package br.com.gbruno.controllers;
+
+import br.com.gbruno.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+}
