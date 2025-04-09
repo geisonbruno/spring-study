f1062b0078537a2e34c8078de1db67e09416a932
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-04-09 14:47:35 -0300

    feat: created method saveProduct - ProductController

--- src/main/java/br/com/gbruno/controllers/ProductController.java
+++ src/main/java/br/com/gbruno/controllers/ProductController.java
@@ -1,7 +1,15 @@
 package br.com.gbruno.controllers;
 
+import br.com.gbruno.dto.ProductRecordDto;
+import br.com.gbruno.models.ProductModel;
 import br.com.gbruno.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +17,14 @@ public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(productRepository.save(productModel));
+    }
+
 }
